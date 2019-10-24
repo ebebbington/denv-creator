0c8573bc105914dc3511affbe2d51c68d03aa4e0
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-10-24 22:14:48 +0100

    added some mre useful functions to the help file

--- docker-environment-creator/test.py
+++ docker-environment-creator/test.py
@@ -13,7 +13,26 @@ print(spaces)
 def get_current_file_name():
     print(__file__)
 
-tabs = 'some text:' + '\tsome text part of the above text'
-print(tabs)
+def write_array_to_file():
+    dockerfile_content = [
+        'FROM nginx:latest',
+        '# Update and install required packages',
+        'RUN apt-get update',
+        'RUN apt-get install vim -y',
+        'COPY ./.docker/config/copytube.conf /etc/nginx/conf.d/copytube.conf',
+        'ENTRYPOINT ["nginx"]',
+        'CMD ["-g","daemon off;"]'
+    ]
+    file = open('test.txt', 'w')
+    for i in dockerfile_content:
+        file.write(i + '\n')
+#write_array_to_file()
 
-print(os.getcwd() + '/../')
+tabs = 'some text:' + '\tsome text part of the above text'
+#print(tabs)
+
+# Get directory of where the user was when they executed the script
+#print(os.getcwd() + '/../')
+
+f = open('test.deleteme', 'w')
+f.remove()
