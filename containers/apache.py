4db444f38193f76e127840bc1dbcb0badaaf8aef
Edward Bebbington <EdwardSBebbington@hotmail.com>
2020-03-02 00:19:05 +0000

    Fixed a few minor issues

--- containers/apache.py
+++ containers/apache.py
@@ -65,7 +65,7 @@ class Apache:
         dockerfile_content: List[str] = [
             'FROM httpd:2.4.33-alpine',
             'RUN apk update; \\',
-            'apk upgrade;',
+            '  && apk upgrade;',
             'COPY apache.conf /usr/local/apache2/conf/demo.apache.conf',
             'RUN echo "Include /usr/local/apache2/conf/apache.conf" \\',
             '  >> /usr/local/apache2/conf/httpd.conf',
