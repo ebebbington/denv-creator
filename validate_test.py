7834a70c8fef16cf5c332b9c64d6c34d2128eb0e
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-21 01:14:50 +0100

    improved test for validate class

--- validate_test.py
+++ validate_test.py
@@ -1,4 +1,23 @@
 import unittest
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+import sys
+from contextlib import contextmanager
+from unittest.mock import patch
+from unittest import mock
+from unittest import TestCase
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
 
 from validate import Validate
 
@@ -16,6 +35,12 @@ class ValidateTest(unittest.TestCase):
         # when dir doesn't exist
         exists = Validate.dir_exists("~/IdontExist")
         self.assertFalse(exists)
+
+    def test_is_valid_dir_name(self):
+        with captured_output() as (out, err):
+            Validate.is_valid_dir_name('Test-Dir')
+            output = out.getvalue().strip()
+            self.assertEqual(output, '')
 
 if __name__ == '__main__':
     unittest.main()
