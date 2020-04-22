8fa73d0385b88ded3f4473d26c5aab9465ebbd7c
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-22 02:24:00 +0100

    Moved src code into a child dir

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ebebbington/denv-creator",
     packages=setuptools.find_packages(),
+    scripts=['./denv-creator/index'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
