6b66b212a0318b6773138efbf7ece22691a41a18
Edward Bebbington <edward.bebbington@intercity.technology>
2020-04-22 02:11:24 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="denv-creator",
+    version="0.0.1",
+    author="Edward Bebbinngton",
+    author_email="EdwardSBebbington@hotmail.com",
+    description="Simple python script to create every file and configurations for multiple docker containers ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebebbington/denv-creator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
